7374da2f078cfa4c89c6d90e5120432a2a57d276
rudo <rudolfnosek.rn@gmail.com>
2024-01-03 19:12:46 +0100

    scrapper uploading to hopsworks

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import hopsworks
 
 def scrape_huntington_beach_conditions(url):
     # Sending a request to the website
@@ -41,13 +42,47 @@ def scrape_huntington_beach_conditions(url):
         print("Failed to retrieve data")
         return None
 
+import datetime
+from pytz import timezone    
+
+
+def reformat_time(time:str,year:int):
+    time_list = time.split()
+    hour, minute = map(int,time_list[3].split(":"))
+    day = int(time_list[1])
+    month = datetime.datetime.strptime(time_list[0],'%B').month
+    if time_list[4] == 'PM' and hour != 12:
+        hour +=12
+    if time_list[4] == 'AM' and hour == 12:
+        hour = 0
+
+    return datetime.datetime(year=year,month=month,day=day,hour=hour,minute=minute)
+
+def reformat_time_la(time:str):
+    los_angeles = timezone('America/Los_Angeles')
+    la_time = datetime.datetime.now(los_angeles)
+    return reformat_time(time,year=la_time.year)
+
+def preprocess(df):
+    df = df.copy()
+    df['datetime'] = df['time'].apply(reformat_time_la)
+    df['quality'] = df['Surf Quality']
+    df = df[['quality','datetime']]
+    df['year'] = df['datetime'].dt.year
+    df['month'] = df['datetime'].dt.month
+    df['day'] = df['datetime'].dt.day
+    df['hour'] = df['datetime'].dt.hour
+    df['minute'] = df['datetime'].dt.minute
+
+    return df
+
 # URL to be scraped
 url = "https://www.huntingtonbeachca.gov/residents/beach_info/livebeachcondition.cfm"
 
-dfa = pd.read_csv('data/huntington.csv',index_col=0)
-# Scrape the table and print it
-dfb = scrape_huntington_beach_conditions(url)
-if dfb is not None:
-    df = pd.concat([dfa,dfb])
-    print(df)
-    df.to_csv('data/huntington.csv')
+df = scrape_huntington_beach_conditions(url)
+if df is not None:
+    project = hopsworks.login()
+    fs = project.get_feature_store()
+    df = preprocess(df)
+    fg = fs.get_feature_group(name="beach_swells_huntington",version=1)
+    fg.insert(df)
