73760db05eacd524b993cafd9b67371dc638922c
rudo <rudolfnosek.rn@gmail.com>
2024-01-06 02:42:43 +0100

    batch inference debugging

--- wave-batch-inference-pipeline/wave-batch-inference-pipline.py
+++ wave-batch-inference-pipeline/wave-batch-inference-pipline.py
@@ -11,6 +11,7 @@ from sklearn.metrics import mean_absolute_error
 import sys 
 import os
 os.chdir('/'.join(os.getcwd().split('/')[:-1]))
+print(os.getcwd())
 from wave_model.LinReg import LinReg
 
 
