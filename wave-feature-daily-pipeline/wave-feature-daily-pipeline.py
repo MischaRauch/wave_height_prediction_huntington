e88eb7ff880cf83d04ef662ca7eebb10f706fee4
MischaRauch <mischa1297@gmx.de>
2024-01-07 09:58:04 +0100

    fix for wave-feature-daily-pipeline

--- wave-feature-daily-pipeline/wave-feature-daily-pipeline.py
+++ wave-feature-daily-pipeline/wave-feature-daily-pipeline.py
@@ -164,6 +164,49 @@ swells_fg = fs.get_or_create_feature_group(name="buoy_swells_huntington",
                 )
 swells_fg.insert(df)
 
+import numpy as np
+from sklearn.linear_model import LinearRegression
+class LinReg(LinearRegression):
+
+    encoding = {'Poor':0.0,
+                'Poor To Fair':1.0,
+                'Fair':2.0,
+                'Fair To Good':3.0}
+
+    def __init__(self):
+        super().__init__()
+
+    def fit(self , X, y, sample_weight = None):
+        y = self.encode(y)
+        return super().fit(X, y, sample_weight)
+
+    def predict_labels(self, X):
+        y =  np.round(super().predict(X))
+        return self.decode(y)
+    
+    def score(self, X, y, sample_weight = None):
+        y = self.encode(y)
+        return super().score(X, y, sample_weight)
+    
+    def encode(self,arr):
+        arr = arr.copy()
+        try:
+            arr = arr.values
+        except AttributeError:
+            pass
+        
+        for key,val in self.encoding.items():
+            arr[arr==key] = val
+        arr = arr.astype(int)
+        return arr
+    
+    def decode(self,arr):
+        arr = arr.copy()
+        arr = arr.astype(str).reshape(-1)
+        for key,val in self.encoding.items():
+            arr[arr==str(val)] = key
+        return arr
+
 import joblib
 
 data = df.sort_values(by='hits_at',ascending=False).head(1)[['height','period','direction','pred_dtime']]
@@ -173,7 +216,6 @@ X = data.drop(['pred_dtime'],axis=1).values
 mr = project.get_model_registry()
 model = mr.get_model('wave_reg',version=1)
 model_dir = model.download()
-print(model_dir)
 model = joblib.load(model_dir + "/wave_reg.pkl")
 
 y_pred = model.predict_labels(X)
